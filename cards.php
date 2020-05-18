aea6c06fa932517bffaee7d24cb9eda1bef55dce
olakehinde <olakehinde59@gmail.com>
2020-05-18 17:34:37 +0100

    cleaned up and formatted code

--- cards.php
+++ cards.php
@@ -3,7 +3,7 @@
 // generate card pins
 function generate($length){
 	$result = '';
-	for ($i=0;$i < $length; $i++){
+	for ($i = 0; $i < $length; $i++){
 		$result.=mt_rand(1,9);
 	}
 	return $result;
@@ -15,9 +15,9 @@ function generate($length){
 // save generated pins into a csv file
 $file = fopen("cards.csv", "w");
 
-for ($i=1; $i<= 60000; $i++){
+for ($i = 1; $i <= 60000; $i++){
 	// $serial = '105019082'; //this line should be edited when generating new serial range 
-	$pin = generate(12);
+	// $pin = generate(12);
 	// $denomation = '050';
 	$raw = [];
 	// $data = $serial. str_pad($i, 7, '0', STR_PAD_LEFT) .',' .generate(12).','.'050';
